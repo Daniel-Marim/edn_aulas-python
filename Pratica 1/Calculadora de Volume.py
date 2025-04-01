31361879282f2ab28c9f3ff00ec70e620a1865e2
Daniel-Marim <daniel.amadormarim@gmail.com>
2025-04-01 03:05:40 -0300

    fix de pasta extra

--- Pratica 1/Calculadora de Volume.py
+++
@@ -1,7 +0,0 @@
-comprimento = float(input("Digite o comprimento em centimetros: "))
-largura = float(input("Digite o largura em centimetros: "))
-altura = float(input("Digite o altura em centimetros: "))
-
-volume = comprimento * largura * altura
-
-print(f"O volume da caixa é: {volume} cm³")
--- Pratica 1/soma.py
+++
@@ -1,7 +0,0 @@
-a = float(input('Digite um valor: '))
-b = float(input('Digite outro valor: '))
-
-print(f"A soma de {a} e {b} é igual a {a + b}")
-print(f"A subtração entre {a} e {b} é igual a {a - b}")
-print(f"A divisão de {a} e {b} é igual a {a / b:.2f}")
-print(f"A multiplicação com {a} e {b} é igual a {a * b}")
